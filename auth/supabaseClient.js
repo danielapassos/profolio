7d81d7cc151e9ae0445b29869d56024222391b3c
Daniela Passos <moitidani@gmail.com>
2023-03-23 10:45:10 -0500

    Update Supabase

--- auth/supabaseClient.js
+++ auth/supabaseClient.js
@@ -1,6 +1,6 @@
 import { createClient } from '@supabase/supabase-js';
 
-const supabaseUrl = 'https://fdbshsyfnzrtfenpvxzw.supabase.co';
+const supabaseUrl = process.env.SUPABASE_URL;
 const supabaseKey = process.env.SUPABASE_ANON_PUBLIC_KEY
 
 const supabase = createClient(supabaseUrl, supabaseKey);
