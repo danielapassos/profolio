8785e71b83af31e722657f347fb6d66f5edcb458
Daniela Passos <moitidani@gmail.com>
2023-03-23 10:38:23 -0500

    Integrating Database

--- auth/getstarted.js
+++ auth/getstarted.js
@@ -1,21 +1,31 @@
 import supabase from './supabaseClient';
 
 async function handleSubmit(event) {
-  event.preventDefault();
-  const { data, error } = await supabase
-  .from('users')
-  .insert({ 
-    name: 'John Doe',
-    email: 'example@example.com', 
-    password: 'mypassword', 
-    role: 'user' 
-  });
+  event.preventDefault(); // prevent form from refreshing the page
 
-  if (error) {
+  const form = event.target;
+  const formData = new FormData(form);
+
+  const name = formData.get('name');
+  const email = formData.get('email');
+  const password = formData.get('password');
+  const role = 'user'; // set user role to "user" by default
+
+
+  try {
+    const { data, error } = await supabase
+      .from('users')
+      .insert({ name, email, password, role });
+
+    if (error) {
+      console.error('Error signing up:', error.message);
+    } else {
+      console.log('Signed up successfully:', data);
+      form.reset(); // reset the form fields
+    }
+  } catch (error) {
     console.error('Error signing up:', error.message);
-  } else {
-    console.log('Signed up successfully:', data);
   }
 }
 
-document.querySelector('form').addEventListener('submit', handleSubmit);
+document.querySelector('#signup-form').addEventListener('submit', handleSubmit);
